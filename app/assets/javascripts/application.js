70c32619c08805bd5e7b0fa8a91f90a297782c35
andrewgy8 <andrewgy8@gmail.com>
2016-06-12 12:33:11 -0400

    Homepgae theme fixed with asset pipeline adjustment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,26 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require_tree ../../../vendor/assets/javascripts
+//= require jquery-1.11.2.min.js
+//= require jquery.easing
+//= require jquery-ui.min.js
+//= require bootstrap.min.js
+//= require jquery.flexslider.js
+//= require jquery.fitvids.js
+//= require jquery.viewportchecker.js
+//= require jquery.stellar.min.js
+//= require wow.min.js
+//= require jquery.colorbox-min.js
+//= require owl.carousel.min.js
+//= require jquery.themepunch.tools.min.js
+//= require jquery.themepunch.revolution.min.js
+//= require isotope.pkgd.min.js
+//= require masonry.pkgd.min.js
+//= require imagesloaded.pkgd.min.js
+//= require jquery.fs.tipper.min.js
+//= require mediaelement-and-player.min.js
+//= require sidebar-menu.js
+//= require revolution-slider.js
+//= require theme.js
+//= require navigation.js
+
